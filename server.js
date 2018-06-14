82258285baba0e0b1b63c83397fb538d92962f1f
mjtedder <mjtedder@gmail.com>
2018-06-14 09:19:23 -0400

    redirect user to view.html when navigating to port 3000

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
@@ -41,7 +42,7 @@ let characters = [{
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", (req, res) => {
-  res.send("Welcome to the Star Wars Page!");
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
 // Displays all characters
