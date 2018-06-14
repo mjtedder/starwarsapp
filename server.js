72e0c73d8084cf30e4a6784e62b8ed33752e4391
mjtedder <mjtedder@gmail.com>
2018-06-14 09:47:30 -0400

    add new character feature complete

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 // Star Wars Characters (DATA)
@@ -31,8 +33,9 @@ let characters = [{
   },
   {
     routeName: "obiwan",
-    name: "Obi-Wan Kenobi",
+    name: "Obi Wan Kenobi",
     role: "Jedi Master",
+    age: 55,
     forcePoints: 1800
   }
 ];
@@ -45,42 +48,44 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
-// Displays all characters
-app.get("/api/:characters?", (req, res) => {
-
-  let chosen = req.params.characters;
-
-  if (chosen) {
-
-    console.log(chosen);
-
-    for (let i = 0; i < characters.length; i++) {
-      if (chosen === characters[i].routeName) {
-        res.json(characters[i]);
-        return;
-      }
-    }
-    res.send("No character found.");
-
-  } else {
-
-    res.json(characters);
-  }
-
+app.get("/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "add.html"));
 });
 
+// Displays all characters
+app.get("/api/:characters", (req, res) => {
+  return res.json(characters);
+});
 
 // Displays a single character, or returns false
+app.get("/api/characters/:character", (req, res) => {
+  let chosen = req.params.character;
+
+  console.log(chosen);
+
+  for (let i = 0; i < characters.length; i++) {
+    if (chosen === characters[i].routeName) {
+      return res.json(characters[i]);
+    }
+  }
+  return res.json(false);
+});
 
 // Create New Characters - takes in JSON input
+app.post("/api/characters", (req, res) => {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  let newcharacter = req.body;
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 
-// req.body hosts is equal to the JSON post sent from the user
-// This works because of our body-parser middleware
+  console.log(newcharacter);
 
+  characters.push(newcharacter);
 
-// Using a RegEx Pattern to remove spaces from newCharacter
-// You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-
+  res.json(newcharacter);
+});
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function () {
